fb8a13a24409617ab6103dd684f9141781c862fc
alfey504 <alfey504@gmail.com>
2025-06-08 00:24:23 -0400

    updated search page

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -14,7 +14,21 @@ export default function Page () {
     const [ pageData, setPageData ] = useState<PageData>({
         searchData: undefined,
         message: "Please enter a query to search",
-    })    
+    })   
+
+    const [ compareCart, setCompareCart ] = useState<Product[]>([])
+
+    const addToCompare = async (product: Product) => {
+        let newCompareCart = compareCart
+        newCompareCart.push(product)
+        console.log("add to compare clicked")
+        setCompareCart(newCompareCart)
+    }
+
+    const isOnCart = (product: Product) : boolean => {
+        console.log( "isoncart -> " + compareCart.length)
+        return (compareCart.includes(product))    
+    }
 
     const onSearch = async () => {
         const searchQuery = searchRef.current?.value
@@ -24,29 +38,28 @@ export default function Page () {
         
         const searchData = await search(searchQuery) 
         if (searchData == undefined) {
-            console.log("uffff failed returned undefined")
             setPageData({
                 searchData: undefined,
-                message: "there was an issue searching"
+                message: "there was an issue searching",
             })
             return
         }
 
-        console.log("Huhhh worked but why not ??")
         setPageData({
             searchData: searchData,
-            message: undefined
+            message: undefined,
         })
     }
 
     return (
         <div>
             <NavBar searchRef={searchRef} onSearch={onSearch}/>
-            <div className="flex flex-row justify-around flex-wrap mt-10 items-center">
+            <ProductsSection className="ml-20 mr-20 flex justify-center" pageData={pageData} addToCompare={addToCompare} isOnCart={isOnCart}/>
+            {/* <div className="flex flex-row justify-around flex-wrap mt-10 items-center">
                 {pageData.searchData?.searchData.map((product) => {
                     return <ProductCard product={product} />
                  })}
-            </div>
+            </div> */}
         </div>
     )
 }
@@ -58,7 +71,7 @@ function NavBar({
 }:{
     className?: string
     searchRef?: React.RefObject< HTMLInputElement | null >
-    onSearch?: () => void
+    onSearch?: () => void,
 }) {
     
     return (
@@ -72,25 +85,67 @@ function NavBar({
     )
 }
 
+function ProductsSection({
+    className,
+    pageData,
+    addToCompare,
+    isOnCart,
+}:{
+    className?: string,
+    pageData: PageData,
+    addToCompare: (product: Product) => Promise<void>,
+    isOnCart: (product: Product) => boolean
+}){
+    return(
+        <div className={className}>
+            <div className="flex justify-center">
+                <div className="flex flex-row justify-start flex-wrap mt-10 items-center">
+                    {pageData.searchData?.searchData.map((product) => {
+                        return <ProductCard product={product}  addToCompare={addToCompare} isOnCart={isOnCart}/>
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
 
 function ProductCard({
     className,
     product,
+    addToCompare,
+    isOnCart,
 }:{
     className?: string,
     product: Product,
+    addToCompare:  (product: Product) => Promise<void>,
+    isOnCart: (product: Product) => boolean
 }) {
 
-    const trimTo = (str: string, len: number, trail: string) => {
-        return str.slice(0, len) + trail
-    }
+    const trimmedTitle = (product.title.length <= 30)? product.title : product.title.slice(0, 30) + "..."
+    const stars = function(): string{
+        const star = "★"
+        const emptyStar = "☆"
+        const rating = Math.floor(product.rating)
+        return star.repeat(rating) + emptyStar.repeat(5-rating)
+    }();
+    const rating = product.rating.toFixed(1);
+    const buttonState = (isOnCart(product))? "Remove" : "Add"
+ 
+    console.log(product.totalOriginalPrice.value + "->" + product.originalPrice.value + "->" + product.totalOriginalPrice.value)
     return (
         <div className={className}>
-            <div className="flex flex-col p-4 border-2  rounded-xl border-gray-200 shadow-lg mt-3 mb-3">
-                <img src={product.images[0].url} width={150} height={150}/>
-                <span className=" text-md font-semibold mt-5 w-40">{trimTo(product.title, 30, "...")}</span>
-                <span className="text-sm">{product.rating} star <span className=" text-sm text-gray-400">{product.ratingsCount}(reviews)</span></span>
-                <span>#{product.skuId}</span>
+            <div className="flex flex-col p-4 border-2  rounded-xl border-gray-200 shadow-lg mt-3 mb-3 ml-3 mr-3 h-96">
+                <div className="flex justify-center items-center w-52 h-52">
+                    <img src={product.images[0].url} width="50%" height="50%"/>
+                </div>
+                <span className=" text-md font-semibold mt-5 w-52">{trimmedTitle}</span>
+                <span className="text-md text-amber-400">{stars} <span className="text-sm text-black">{rating}</span> <span className=" text-sm text-gray-400">({product.ratingsCount})</span></span>
+                <span className="text-sm text-gray-400">#{product.skuId}</span>
+                <span>${product.currentPrice.value}</span> 
+                <div className="flex flex-row justify-around items-center mt-5 mb-5">
+                    <button className=" bg-green-800  text-white pt-2 pb-2 pl-3 pr-3 rounded-lg" onClick={() => { addToCompare(product)}}>{buttonState}</button>
+                    <button className="bg-red-500 text-white pt-2 pb-2 pl-3 pr-3 rounded-lg" onClick={ ()=> { window.location.href = "https://www.canadiantire.ca"}} >Website</button>
+                </div>
             </div>
         </div>
     )
